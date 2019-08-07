4ce5a99112d7ceb9cb2e5c74eb9d1f36eb4804e7
Juan Pablo Tamayo <jtamayoh@gmail.com>
2019-08-07 17:12:42 -0500

    chapter006

--- microblog.py
+++ microblog.py
@@ -2,9 +2,10 @@ from app import app, db
 from app.models import User, Post
 
 
-app.run(debug=True)
-
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
