583d76629416abe0313eac248a15193a02471e39
Juan Pablo Tamayo <jtamayoh@gmail.com>
2019-08-07 16:33:48 -0500

    chapter005

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# Create Flask app
 app = Flask(__name__)
+
+# Apply configuration
 app.config.from_object(Config)
+
+# Add flask extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' #name of the login view (the name you would use in a url_for())
 
 from app import routes, models
